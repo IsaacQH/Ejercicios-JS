a4975e5738396a6d35630f3d5a8082bcddee7fad
IsaacQH <isaacqwert@gmail.com>
2023-07-15 00:25:43 -0600

    CORRECIÓN DE CARPETAS

--- Hello World/hello-world.js
+++
@@ -1,5 +0,0 @@
-
-alert("Llamando archivo: Hello World JS");            //Crea alertas en ventana emergente
-document.write("Hola mundo desde fichero externo")    //Escribe en el documento el valor indicado después de ejecutar
-console.log("Muestra esto en la consola")             //Nos muestra en la consola del navegador (apartado como matlab) lo que se escriba
-console.log(88+4)
