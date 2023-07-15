2a192adf9753fa96bba4766a6d92efecf473b831
IsaacQH <isaacqwert@gmail.com>
2023-07-15 03:25:50 -0600

    02-variables.js: Método strict y variables var let

--- Codigos_JS/02-variables.js
+++ Codigos_JS/02-variables.js
@@ -1,9 +1,13 @@
 
+'use strict'                    //Método que nos permite restringir malos abitos de programacion como declara variables sin var o let
+
 // Variables: Es un contenedor de información
 
-var pais = "España";
-var continente = "Europa";
+var pais = "España";                               //Definir variables en var
+var continente = "Europa"; 
 var antiguedad = 2019;
-var pais_continente = pais +" "+ continente
+var pais_continente = pais +" "+ continente;
+
+let prueba = "hola";                              //Definir variables en let
 
 console.log(pais, continente, antiguedad, pais_continente);
