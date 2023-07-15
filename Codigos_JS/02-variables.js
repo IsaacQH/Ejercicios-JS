e2e7d76c4790fb6a540745d1d7c31754164efb47
IsaacQH <isaacqwert@gmail.com>
2023-07-15 01:15:06 -0600

    2-variables.js: Utilizando variables

---
+++ Codigos_JS/02-variables.js
@@ -0,0 +1,9 @@
+
+// Variables: Es un contenedor de información
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 2019;
+var pais_continente = pais +" "+ continente
+
+console.log(pais, continente, antiguedad, pais_continente);
