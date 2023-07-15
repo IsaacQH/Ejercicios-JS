1cea4f8308234b54c09b51a04c7b860765a095ea
IsaacQH <isaacqwert@gmail.com>
2023-07-14 19:28:30 -0600

    Ejercicio Hello World: Completo

---
+++ Hello World/hello-world.js
@@ -0,0 +1,3 @@
+
+alert("Llamando archivo: Hello World JS");            //Crea alertas en ventana emergente
+document.write("Hola mundo desde fichero externo")    //Escribe en el documento el valor indicado después de ejecutar
