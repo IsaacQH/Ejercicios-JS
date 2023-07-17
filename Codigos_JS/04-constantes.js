7fc90f9c8cc4d99a66d36dd5936b5b15a9f31bd1
IsaacQH <isaacqwert@gmail.com>
2023-07-17 13:58:11 -0600

    04-constantes.js: Completo

---
+++ Codigos_JS/04-constantes.js
@@ -0,0 +1,9 @@
+'use strict'             //Activa el modo estricto para mejor práctica de programación
+
+//CONSTANTES
+//Una constante es una variable que se declará y su valor no va a cambiar.
+
+var web = "http://isaacqh.es"     //Declaración de variable
+const ip = "192.07.0.82"         //Declaración de constante
+
+
