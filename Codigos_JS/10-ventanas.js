765366afb5fae081e290614a39b767f6d8e01283
IsaacQH <isaacqwert@gmail.com>
2023-07-18 00:27:03 -0600

    10-ventanas.js: Completo

---
+++ Codigos_JS/10-ventanas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//ALERT
+alert("Esta es mi alerta!!")                                    //Nos permite mostrar una ventana de alerta en la página web
+
+//CONFIRM                                                       //Nos muestra una pantalla para confirmar selección
+var resultado = confirm("¿Seguro de que quieres continuar?")    //Guarda el valor T/F según la respuesta del usuario en la web
+console.log(resultado)                                          //Muestra en consola el valor de resultado
+
+//INGRESO DE DATOS
+var edad = prompt("Ingrese la edad")                        //Ventana emergente y se ingresa la edad, valor default es 18
+console.log(edad)                                               //Se muestra en consola el valor de edad
