3ef75cbec421aab82f32fa434ac91b90aa381de2
IsaacQH <isaacqwert@gmail.com>
2023-07-17 20:01:38 -0600

    08-bucle-for.js: Completo

---
+++ Codigos_JS/08-bucle-for.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//BUCLE FOR:
+
+//Un bucle es una estructura de control que se repite varias veces.
+
+//Crearemos un bucle que cuente del 0 al 100
+var numero = 100
+
+for(var i = 0; i <= numero; i++){       //Se inicializa el for con i desde cero y que el bucle acabe hasta 100 incluido, con aumento de +1
+    console.log(i);
+
+//    debugger;                        //Nos permite en navegador detener el programa al correr para poder debuggear el ciclo
+
+}
+
+
