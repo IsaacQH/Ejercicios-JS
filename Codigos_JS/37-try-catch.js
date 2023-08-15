b7a17e37c18d1b46977b0417d9760a4b3b04d6f7
IsaacQH <isaacqwert@gmail.com>
2023-08-14 23:18:58 -0600

    37-try-catch.js: completo

---
+++ Codigos_JS/37-try-catch.js
@@ -0,0 +1,15 @@
+'use strict'
+
+addEventListener("load", () => {
+
+    try{                                    //se inicia el try
+        var year = 2019                     //declaracion de varaible
+        alert (yea)                         //Se escribe mal la variable year
+
+        console.log(decodeURIComponent("www.google.com"))      //Imprime en consola el url comprobado. decodeURI revisa que exista el URL
+
+    }catch(error){                         //Si hay un error en try, cacha el error
+        console.log(error)                 //Imprime en consola el error
+    }
+
+})
